596033af2f05f9eb085d9d375cf1b0ba6524d309
Stallix <team.evolutionx.rom@gmail.com>
2019-10-19 08:29:20 +0000

    vendor: branding: Bump version to 2.3
    We love you, Pie

--- config/branding.mk
+++ config/branding.mk
@@ -1,6 +1,6 @@
 # Versioning System
-EVO_BASE_VERSION = 2.2
-EVO_CODENAME := BeforeTen
+EVO_BASE_VERSION = 2.3
+EVO_CODENAME := ByebyeMrAndroidPie
 
 # Set all versions
 CUSTOM_BUILD_TYPE ?= UNOFFICIAL
